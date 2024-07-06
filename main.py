e4bcc8155bae44c8257c30ce8e49cc9e7ed9a099
BBernYY <66414852+BBernYY@users.noreply.github.com>
2024-07-06 18:20:20 +0200

    fixed bug where aspect ratio wuould be bad

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def to_sigma(name, log=True):
         f = f.convert('RGB')
         pixel_map = f.load()
         width, height = f.width, f.height
-    for x in range(width):
-        for y in range(height):
+    for y in range(height):
+        for x in range(width):
             pixel = pixel_map[x,y]
             out += webcolors.rgb_to_name(tuple(closest(list(pixel))[0]))+"\t"
         out = out[:-1]+"\n"
@@ -39,19 +39,17 @@ def from_sigma(name):
         f2 = f.read().replace('\x00', '').replace('\n\n', '\n')
         data = [i.split('\t') for i in f2.split("\n")]
         data2: list[tuple[int, int, int]] = np.full((*np.shape(data), 3), (0.0, 0.0, 0.0), dtype=int)
-    for y in range(len(data)):
-        for x in range(len(data[y])):
-            a = data[y][x]
+    for x in range(len(data)):
+        for y in range(len(data[x])):
+            a = data[x][y]
             a2 = tuple(webcolors.name_to_rgb(a))
             # ans = rgb_to_hsv(a2[0]/255, a2[1]/255, a2[2]/255)
             # ans2 = hsv_to_rgb(ans[0], ans[1], ans[2])
             # data2[y][x] = int(ans2[0]*255), int(ans2[1]*255), int(ans2[2]*255)
             # OMG I LITERALLY WASTED TWO HOURS ON THIS AND THE IMAGE WAS JUST FLIPPED
             # im gonna keep this here out of spite
-            data2[y][x] = a2
+            data2[x][y] = a2
     img = Image.fromarray(np.array(data2.astype(np.uint8)))
-    img = img.rotate(-90)
-    img = img.transpose(Image.FLIP_LEFT_RIGHT)
     img.save(path.splitext(name)[0]+'_out.jpg')
 NAME = "assets\\biden.png"
 with open(path.splitext(NAME)[0]+".sigma", 'w') as f:
